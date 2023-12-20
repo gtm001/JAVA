cc279c2769be6307b0a5b8b49742c7f535ba49db
gautam kumar <119605352+gtm001@users.noreply.github.com>
2023-12-21 02:13:15 +0530

    Add files via upload
    information about the variable in Java.

---
+++ variable.java
@@ -0,0 +1,12 @@
+// --Variable In JAVA--
+// Q.take a=10 and b=25 and operation +,-,/.
+class variable{
+    public static void main(String[] arg){
+        int a=10;
+        int b=25;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.print(a/b); //it'll give output as integer.
+    }
+    
+}
