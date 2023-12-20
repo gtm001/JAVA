f3db39c60caad930a9aa565a86e49fcd21d466a0
gautam kumar <119605352+gtm001@users.noreply.github.com>
2023-12-21 02:09:45 +0530

    Add files via upload
    Some information about the data type.

---
+++ datatype.java
@@ -0,0 +1,12 @@
+// --JAVA IS A TYPED DATATYPE
+/*There are two types datatype in java
+1.primitive                                   2.Non-primitive 
+a.int (4 byte)                                     a.String
+b.char (2 byte)                               b.Array
+c.float (4 byte)                              c.Class
+d.double (8 byte)                             d.Object
+e.long (8 byte)                               e.Interface
+f.boolean (1 byte)
+g.short (2 byte)
+h.byte (1 byte)
+                                                                        */
