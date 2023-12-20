a9947003d7b842f60015ac0db82aff22b828fe1d
gautam kumar <119605352+gtm001@users.noreply.github.com>
2023-12-21 02:15:09 +0530

    Add files via upload
    Steps to take input in variable.

---
+++ input.java
@@ -0,0 +1,28 @@
+// --- INPUT IN JAVA---
+// import java.util.*;// first you have to import this!
+// class input{
+//     public static void main(String  args[]){
+//     Scanner sc= new Scanner(System.in); //here you hve to define the object.
+//     String name = sc.nextLine();
+//     /* here if we only use 'next' then it will output a single word only!
+//     we can also use 'nextInt', 'nextFloat', 'nextChar'  forr differernt datatypes!*/
+//     System.out.println(name);
+//     }
+// }
+
+// Q.Take two variable a & b and print their sum.
+
+// --Solution--
+// import java.util.*;
+// class input{
+//     public static void main(String args[]){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("enter a: ");
+//         int a = sc.nextInt();
+//         System.out.print("enter b: ");
+//         int b = sc.nextInt();
+ 
+//         System.out.print("The sum of a & b is: ");
+//         System.out.print(a+b);
+//     }
+// }
