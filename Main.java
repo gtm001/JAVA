3979c6ffdf04ae678b3e9bbd952171b30f8af7bc
gautam kumar <119605352+gtm001@users.noreply.github.com>
2023-12-21 02:08:34 +0530

    Add files via upload
    basic about the syntax(output syntax) of java.

---
+++ Main.java
@@ -0,0 +1,39 @@
+// -----OUTPUT IN JAVA --------
+
+
+// class Main{
+//     public static void main(String args[]){
+//         // output in java 
+//         System.out.println("Hellow World!");// 'ln' used for the next line!
+//         System.out.print("Are you excited!?\n");//'\n' for the next line!
+
+//     }
+// }
+
+// ---question--
+/*
+
+*
+**
+***
+****
+
+*/
+
+// Always remember that the class name must be exactly same as the  file name!
+
+//---Solution!---
+// class Main{
+//     public static void main(String args[]){
+//         // System.out.println("*");
+//         // System.out.print("**\n");
+//         // System.out.println("***");
+//         // System.out.print("****");
+//         //   OR
+//         System.out.print("*\n**\n***\n****"); //we can do it in a single line also!
+//     }
+    
+// }
+
+
+
